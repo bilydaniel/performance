7fc6b6a14bb27e4e5f6f2780bcabee10e4281415
Daniel <bilidanek@seznam.cz>
2025-02-01 17:34:09 +0100

    asd

--- haversine/src/zson.zig
+++ haversine/src/zson.zig
@@ -9,10 +9,18 @@ pub const HaversinePair = struct {
     y1: f64,
 };
 
+const JsonParseError = error{
+    UnexpectedToken,
+    ExpectedColon,
+    ExpectedComma,
+    AllocationFailed,
+    OutOfMemory,
+};
+
 const JsonParser = struct {
     source: *std.ArrayList(u8),
     at: u64 = 0,
-    hadError: u32 = 0,
+    hadError: bool = false,
     allocator: std.mem.Allocator,
 
     pub fn init(allocator: std.mem.Allocator, input: *std.ArrayList(u8)) !JsonParser {
@@ -20,7 +28,7 @@ const JsonParser = struct {
             .allocator = allocator,
             .source = input,
             .at = 0,
-            .hadError = 0,
+            .hadError = false,
         };
     }
 
@@ -28,35 +36,37 @@ const JsonParser = struct {
         const tokenstart = this.at;
         this.at += 1;
 
-        if (rest.len > this.source.items.len - this.at) {
-            return error.KeywordTooShort;
+        if (rest.len <= this.source.items.len - this.at) {
+            const check = this.source.items[this.at .. this.at + rest.len];
+            if (std.mem.eql(u8, check, rest)) {
+                this.at += rest.len;
+                const tokenend = this.at;
+
+                result.type = tokentype;
+                result.value = this.source.items[tokenstart..tokenend];
+            }
         }
-
-        const check = this.source.items[this.at .. this.at + rest.len];
-        if (!std.mem.eql(u8, check, rest)) {
-            return error.IncorrectKeyword;
-        }
-
-        this.at += rest.len;
-        const tokenend = this.at;
-
-        result.type = tokentype;
-        result.value = this.source.items[tokenstart..tokenend];
     }
 
-    pub fn getJsonToken(this: *JsonParser) !JsonToken {
+    fn IsJsonDigit(this: *JsonParser) bool {
+        var result = false;
+
+        if (this.at < this.source.items.len) {
+            const val = this.source.items[this.at];
+            result = (val >= '0' and val <= '9');
+        }
+        return result;
+    }
+
+    pub fn getJsonToken(this: *JsonParser) JsonToken {
         var result: JsonToken = undefined;
-        //var value = std.ArrayList(u8).init(this.allocator);
-        //var result: JsonToken = JsonToken{ .type = JsonTokenType.Token_error, .value = &value };
+        result.value = "";
 
         while (this.isWhiteSpace()) {
             this.at += 1;
         }
 
         if (this.at < this.source.items.len) {
-            //result.value = this.source.items[this.at];
-            //try result.value.append(this.source.items[this.at]);
-
             const val = this.source.items[this.at];
             switch (val) {
                 '{' => {
@@ -122,13 +132,46 @@ const JsonParser = struct {
                 '7',
                 '8',
                 '9',
-                => {},
+                => {
+                    result.type = JsonTokenType.Token_number;
+                    const numberstart = this.at;
+
+                    if (val == '-') {
+                        this.at += 1;
+                    }
+
+                    if (val != '0') {
+                        while (this.IsJsonDigit()) {
+                            this.at += 1;
+                        }
+                    }
+
+                    if (this.at < this.source.items.len and this.source.items[this.at] == '.') {
+                        this.at += 1;
+                        while (this.IsJsonDigit()) {
+                            this.at += 1;
+                        }
+                    }
+
+                    if (this.at < this.source.items.len and (this.source.items[this.at] == 'e' or this.source.items[this.at] == 'E')) {
+                        this.at += 1;
+
+                        if (this.at < this.source.items.len and (this.source.items[this.at] == '+' or this.source.items[this.at] == '-')) {
+                            this.at += 1;
+                        }
+
+                        while (this.IsJsonDigit()) {
+                            this.at += 1;
+                        }
+                    }
+
+                    const numberend = this.at;
+                    result.value = this.source.items[numberstart..numberend];
+                },
                 else => result.type = JsonTokenType.Token_error,
             }
         }
 
-        print("JSON_TOKEN: {}\n", .{result});
-        print("JSON_TOKEN_VALUE: {s}\n", .{result.value});
         return result;
     }
 
@@ -141,14 +184,103 @@ const JsonParser = struct {
         return val == ' ' or val == '\t' or val == '\n' or val == '\r';
     }
 
-    //pub fn parseJsonElement(this: JsonParser, label: std.ArrayList(u8), value: JsonToken) !JsonElement {}
+    pub fn inBounds(this: *@This()) bool {
+        return (this.at < this.source.items.len);
+    }
+
+    pub fn isParsing(this: *@This()) bool {
+        return (!this.hadError and this.inBounds());
+    }
+
+    pub fn Error(this: *@This(), token: JsonToken, msg: []const u8) void {
+        this.hadError = true;
+        std.debug.print("ERROR: parser: {}, token:{}, msg:{s}\n", .{ this, token, msg });
+    }
+
+    pub fn parseJsonList(this: *@This(), endtoken: JsonTokenType, hasLabel: bool) JsonParseError!?*JsonElement {
+        var firstElement: ?*JsonElement = null;
+        var lastElement: ?*JsonElement = null;
+        var label: []const u8 = "";
+
+        while (this.isParsing()) {
+            var val = this.getJsonToken();
+            if (hasLabel) {
+                std.debug.print("DEBUG_VALUE: {}", .{val});
+                if (val.type == JsonTokenType.Token_string_literal) {
+                    label = val.value;
+                    const colon = this.getJsonToken();
+                    if (colon.type == JsonTokenType.Token_colon) {
+                        val = this.getJsonToken();
+                    } else {
+                        this.Error(colon, "Expected colon");
+                    }
+                } else {
+                    this.Error(val, "Expected string literal");
+                }
+            }
+
+            const element = try this.parseJsonElement(label, val);
+            if (element) |elem| {
+                if (lastElement != null) {
+                    lastElement.?.nextSibling = elem;
+                    lastElement = elem;
+                } else {
+                    firstElement = elem;
+                    lastElement = elem;
+                }
+            } else if (val.type == endtoken) {
+                break;
+            } else {
+                this.Error(val, "Unexpected value in json");
+            }
+
+            const comma = this.getJsonToken();
+            if (comma.type == endtoken) {
+                break;
+            } else if (comma.type != JsonTokenType.Token_comma) {
+                this.Error(comma, "Unexpected token");
+            }
+        }
+        return firstElement;
+    }
+
+    pub fn parseJsonElement(this: *@This(), label: []const u8, value: JsonToken) JsonParseError!?*JsonElement {
+        var valid = true;
+
+        var subElement: ?*JsonElement = null;
+
+        if (value.type == JsonTokenType.Token_open_bracket) {
+            subElement = try this.parseJsonList(JsonTokenType.Token_close_bracket, false);
+        } else if (value.type == JsonTokenType.Token_open_brace) {
+            subElement = try this.parseJsonList(JsonTokenType.Token_close_brace, true);
+        } else if ((value.type == JsonTokenType.Token_string_literal) or
+            (value.type == JsonTokenType.Token_true) or
+            (value.type == JsonTokenType.Token_false) or
+            (value.type == JsonTokenType.Token_null) or
+            (value.type == JsonTokenType.Token_number))
+        {} else {
+            valid = false;
+        }
+
+        var result: ?*JsonElement = undefined;
+
+        if (valid) {
+            result = try this.allocator.create(JsonElement);
+            result.?.label = label;
+            result.?.value = value.value;
+            result.?.firstSubElement = subElement;
+            result.?.nextSibling = null;
+        }
+
+        return result;
+    }
 };
 
 const JsonElement = struct {
-    label: std.ArrayList(u8),
-    value: std.ArrayList(u8),
-    firstSubElement: *JsonElement,
-    nextSibling: *JsonElement,
+    label: []const u8,
+    value: []const u8,
+    firstSubElement: ?*JsonElement,
+    nextSibling: ?*JsonElement,
 };
 
 const JsonValue = union(enum) {
@@ -182,7 +314,6 @@ const JsonTokenType = enum {
 
 const JsonToken = struct {
     type: JsonTokenType,
-    //value: *std.ArrayList(u8),
     value: []const u8,
 };
 
@@ -209,20 +340,23 @@ fn parseJSON(input: *std.ArrayList(u8)) !void { // !JsonElement
 
     var json_parser = try JsonParser.init(allocator, input);
 
-    const json_token = try json_parser.getJsonToken();
-    print("{}\n", .{json_token});
+    const json_token = json_parser.getJsonToken();
+    //print("{}\n", .{json_token});
 
-    //const result = json_parser.parseJsonElement(null, json_token);
+    const result = try json_parser.parseJsonElement("", json_token);
+
+    print("RESULT_JSON: {?}\n", .{result});
 }
 
 pub fn parseHaversinePairs(input: *std.ArrayList(u8), parsed_values: std.ArrayList(HaversinePair)) !u64 {
-    //TODO: I need to initialize all the arraylists, figure out when and how
     var pair_count: u64 = 0;
     _ = parsed_values;
     pair_count += 1;
 
     const JSON = try parseJSON(input);
     std.debug.print("JSON: {}\n", .{JSON});
+
+    const pairsArray 
 
     return pair_count;
 }
