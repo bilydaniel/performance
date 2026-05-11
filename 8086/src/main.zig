45d4c01e4d24cc935e0a435e6af7ed1c4c196cd2
HP <daniel.bily@firma.seznam.cz>
2026-05-11 19:21:23 +0200

    asd

--- 8086/src/main.zig
+++ 8086/src/main.zig
@@ -22,7 +22,60 @@ pub fn main() !void {
         std.log.err("Wrong number of arguments\n", .{});
         return error.wrongNumberOfArguments;
     }
+
+    const cwd = std.fs.cwd();
+    const fileName = args[1];
+
+    const MB = 1024 * 1024;
+    instructions = try cwd.readFileAlloc(arenaAllocator, fileName, 1 * MB);
+
+    var buffer: [32]u8 = undefined;
+    var writer = std.Io.Writer.fixed(&buffer);
+
+    while (true) {
+        writer.end = 0;
+        const byte1 = getNext() orelse break;
+        std.debug.print("{b}\n", .{byte1});
+
+        const d = byte1 & 0b00000010; // 0 = reg is source 1 = reg is dest
+        const w = byte1 & 0b00000001; // 0 = 8  1 = 16
+        _ = d;
+        _ = w;
+        if ((byte1 & 0b11111100) == 0b10001000) {
+            //mov
+            try writer.writeAll("MOV ");
+
+            const byte2 = getNext() orelse undefined;
+            // 00 = memory, 01 memory 8 displace, 10 memory 16 displace, 11 register
+            const mod = byte2 & 0b11000000;
+            if (mod == 0b11000000) {
+                //registers
+
+            }
+
+            const reg = byte2 & 0b00111000;
+            const rm = byte2 & 0b00000111;
+        }
+
+        std.debug.print("write: {s}", .{buffer[0..writer.end]});
+    }
+
+    //const value: u8 = @intCast(file[0]);
 }
+
+var instructions: []u8 = undefined;
+var instructionIndex: usize = 0;
+pub fn getNext() ?u8 {
+    if (instructionIndex >= instructions.len) {
+        return null;
+    }
+    const instruction = instructions[instructionIndex];
+    instructionIndex += 1;
+    return instruction;
+}
+
+//TODO: @finish
+//pub fn getRegister(){}
 
 pub fn printArgs(args: [][:0]u8) void {
     for (args) |arg| {
