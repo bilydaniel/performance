7a859848b21837aa6d0b406bf9c1ca8822809e56
HP <daniel.bily@firma.seznam.cz>
2026-05-13 20:04:56 +0200

    asd

--- 8086/build.zig
+++ 8086/build.zig
@@ -59,7 +59,7 @@ pub fn build(b: *std.Build) void {
     // don't need and to put everything under a single module.
     const exe = b.addExecutable(.{
         .use_llvm = true, // for debugging
-        .name = "testing_grounds",
+        .name = "8086",
         .root_module = b.createModule(.{
             // b.createModule defines a new module just like b.addModule but,
             // unlike b.addModule, it does not expose the module to consumers of
--- 8086/src/main.zig
+++ 8086/src/main.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 
 pub fn main() !void {
+    const debug: bool = true;
     var gpa: std.heap.GeneralPurposeAllocator(.{}) = .{};
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
@@ -38,23 +39,96 @@ pub fn main() !void {
 
     while (true) {
         const byte1 = getNext() orelse break;
+        var instruction = Instruction.init(byte1);
+        if (debug) {
+            var buf: [20]u8 = undefined;
+            const op = try std.fmt.bufPrint(&buf, "op: {b}\n", .{byte1});
 
+            _ = try aw.writer.write(op);
+        }
+
+        //std.debug.print("{s}", .{buffer[0..writer.end]});
+        buffer = aw.toArrayList();
+        std.debug.print("{s}", .{buffer.items});
+    }
+}
+
+var instructions: []u8 = undefined;
+var instructionIndex: usize = 0;
+pub fn getNext() ?u8 {
+    if (instructionIndex >= instructions.len) {
+        return null;
+    }
+    const instruction = instructions[instructionIndex];
+    instructionIndex += 1;
+    return instruction;
+}
+
+const registerTable = [2][8][]const u8{
+    // w = 0 (8-bit registers)
+    .{ "al", "cl", "dl", "bl", "ah", "ch", "dh", "bh" },
+    // w = 1 (16-bit registers)
+    .{ "ax", "cx", "dx", "bx", "sp", "bp", "si", "di" },
+};
+pub fn getRegister(allocator: std.mem.Allocator, reg: usize, w: u8) ![]u8 {
+    //allocation not necesery, just want to get used to arenas
+    const register = registerTable[w][reg];
+    const result = try std.fmt.allocPrint(allocator, "{s}", .{register});
+    return result;
+}
+
+pub fn printArgs(args: [][:0]u8) void {
+    for (args) |arg| {
+        std.debug.print("{s}\n", .{arg});
+    }
+}
+
+const calcTable = [8][]const u8{ "bx + si", "bx + di", "bp + si", "bp + di", "si", "di", "bp", "bx" };
+pub fn getAddressCalc(allocator: std.mem.Allocator, rm: usize) ![]u8 {
+    //allocation not necesery, just want to get used to arenas
+    const calc = calcTable[rm];
+    const result = try std.fmt.allocPrint(allocator, "{s}", .{calc});
+    return result;
+}
+
+pub fn getData(w: u8) u16 {
+    var imm: u16 = 0;
+    if (w == 0) {
+        const data1 = getNext() orelse undefined;
+        imm = data1;
+    } else {
+        const data1 = getNext() orelse undefined;
+        const data2: u16 = getNext() orelse undefined;
+        const data = (data1) | (data2 << 8);
+        imm = data;
+    }
+
+    return imm;
+}
+
+const Op = enum { mov };
+const OpType = enum { rmtorm };
+const Instruction = struct {
+    op: Op,
+    opType: OpType,
+
+    pub fn init(byte1: u8) !Instruction {
+        //TODO: @finish
         if ((byte1 & 0b11111100) == 0b10001000) {
+            //mov rm to/from rm
             _ = try aw.writer.write("mov ");
 
             const d = (byte1 & 0b00000010) >> 1; // 0 = reg is source 1 = reg is dest
             const w = byte1 & 0b00000001; // 0 = 8  1 = 16
-            //mov
 
             const byte2 = getNext() orelse undefined;
             // 00 = memory, 01 memory 8 displace, 10 memory 16 displace, 11 register
             const mod = byte2 & 0b11000000;
             const reg = byte2 & 0b00111000;
+            const rm = byte2 & 0b00000111;
 
             const regValue = reg >> 3;
             const regLabel = getRegister(arenaAllocator, regValue, w) catch undefined;
-
-            const rm = byte2 & 0b00000111;
             if (mod == 0b11000000) {
                 //registers
                 const rmLabel = getRegister(arenaAllocator, rm, w) catch undefined;
@@ -69,11 +143,63 @@ pub fn main() !void {
                 }
 
                 _ = try aw.writer.write("\n");
+            } else if (mod == 0b00000000) {
+                //TODO:dont forget the 110 rm
+                const rmLabel = getAddressCalc(arenaAllocator, rm) catch undefined;
+
+                if (d == 0) {
+                    _ = try aw.writer.write("[");
+                    _ = try aw.writer.write(rmLabel);
+                    _ = try aw.writer.write("], ");
+                    _ = try aw.writer.write(regLabel);
+                } else {
+                    _ = try aw.writer.write(regLabel);
+                    _ = try aw.writer.write(", [");
+                    _ = try aw.writer.write(rmLabel);
+                    _ = try aw.writer.write("]");
+                }
+                _ = try aw.writer.write("\n");
+            } else if (mod == 0b01000000) {
+                const rmLabel = getAddressCalc(arenaAllocator, rm) catch undefined;
+                const disp = getData(0);
+
+                if (d == 0) {
+                    _ = try aw.writer.write("[");
+                    _ = try aw.writer.write(rmLabel);
+                    _ = try aw.writer.print(" + {}", .{disp});
+                    _ = try aw.writer.write("], ");
+                    _ = try aw.writer.write(regLabel);
+                } else {
+                    _ = try aw.writer.write(regLabel);
+                    _ = try aw.writer.write(", [");
+                    _ = try aw.writer.write(rmLabel);
+                    _ = try aw.writer.print(" + {}", .{disp});
+                    _ = try aw.writer.write("]");
+                }
+                _ = try aw.writer.write("\n");
+            } else if (mod == 0b10000000) {
+                const rmLabel = getAddressCalc(arenaAllocator, rm) catch undefined;
+                const disp = getData(1);
+
+                if (d == 0) {
+                    _ = try aw.writer.write("[");
+                    _ = try aw.writer.write(rmLabel);
+                    _ = try aw.writer.print(" + {}", .{disp});
+                    _ = try aw.writer.write("], ");
+                    _ = try aw.writer.write(regLabel);
+                } else {
+                    _ = try aw.writer.write(regLabel);
+                    _ = try aw.writer.write(", [");
+                    _ = try aw.writer.write(rmLabel);
+                    _ = try aw.writer.print(" + {}", .{disp});
+                    _ = try aw.writer.write("]");
+                }
+                _ = try aw.writer.write("\n");
             }
         } else if ((byte1 & 0b11111110) == 0b11000110) {
+            //mov imm to rm
             //TODO: @finish
-            //immedeate to r/m
-            _ = try aw.writer.write("mov ");
+            _ = try aw.writer.write("mov UNDEFINED\n");
             const w = byte1 & 0b00000001; // 0 = 8  1 = 16
 
             const byte2 = getNext() orelse undefined;
@@ -86,7 +212,6 @@ pub fn main() !void {
                 const rmLabel = getRegister(arenaAllocator, rm, w) catch undefined;
                 _ = try aw.writer.write(rmLabel);
                 _ = try aw.writer.write(", ");
-                //_ = try aw.writer.write(imm);
 
                 _ = try aw.writer.write("\n");
             }
@@ -96,73 +221,114 @@ pub fn main() !void {
             if (w == 0) {
                 const immByte1 = getNext() orelse undefined;
                 imm = @intCast(immByte1);
-                std.debug.print("imm: {}\n", .{imm});
             } else {
                 const immByte1 = getNext() orelse undefined;
                 imm = @intCast(immByte1);
-                std.debug.print("imm: {}\n", .{imm});
             }
         } else if ((byte1 & 0b11110000) == 0b10110000) {
+            //mov imm to r
             _ = try aw.writer.write("mov ");
             const w = (byte1 & 0b00001000) >> 3;
             const reg = (byte1 & 0b00000111);
+
             const regLabel = try getRegister(arenaAllocator, reg, w);
-            _ = try aw.writer.write(regLabel);
-            _ = try aw.writer.write(", ");
 
             var imm: u16 = 0;
-
-            if (w == 0) {
-                const data1 = getNext() orelse undefined;
-                imm = data1;
-            } else {
-                const data1 = getNext() orelse undefined;
-                const data2: u16 = getNext() orelse undefined;
-                const data = (data1) | (data2 << 8);
-                //std.debug.print("data: {}\n", .{data});
-                imm = data;
-            }
-
+            imm = getData(w);
             var immBuff: [20]u8 = undefined;
             const immString = try std.fmt.bufPrint(&immBuff, "{d}", .{imm});
+
+            _ = try aw.writer.write(regLabel);
+            _ = try aw.writer.write(", ");
             _ = try aw.writer.write(immString);
             _ = try aw.writer.write("\n");
+        } else if ((byte1 & 0b11111100) == 0b00000000) {
+            //add rm + rm
+            _ = try aw.writer.write("add ");
+
+            const d = (byte1 & 0b00000010) >> 1;
+            const w = byte1 & 0b00000001;
+
+            const byte2 = getNext() orelse undefined;
+
+            const mod = byte2 & 0b11000000;
+            const reg = byte2 & 0b00111000;
+            const rm = byte2 & 0b00000111;
+
+            const regValue = reg >> 3;
+            const regLabel = getRegister(arenaAllocator, regValue, w) catch undefined;
+            if (mod == 0b11000000) {
+                //registers
+                const rmLabel = getRegister(arenaAllocator, rm, w) catch undefined;
+                if (d == 0) {
+                    _ = try aw.writer.write(rmLabel);
+                    _ = try aw.writer.write(", ");
+                    _ = try aw.writer.write(regLabel);
+                } else {
+                    _ = try aw.writer.write(regLabel);
+                    _ = try aw.writer.write(", ");
+                    _ = try aw.writer.write(rmLabel);
+                }
+
+                _ = try aw.writer.write("\n");
+            } else if (mod == 0b00000000) {
+                //TODO:dont forget the 110 rm
+                const rmLabel = getAddressCalc(arenaAllocator, rm) catch undefined;
+
+                if (d == 0) {
+                    _ = try aw.writer.write("[");
+                    _ = try aw.writer.write(rmLabel);
+                    _ = try aw.writer.write("], ");
+                    _ = try aw.writer.write(regLabel);
+                } else {
+                    _ = try aw.writer.write(regLabel);
+                    _ = try aw.writer.write(", [");
+                    _ = try aw.writer.write(rmLabel);
+                    _ = try aw.writer.write("]");
+                }
+                _ = try aw.writer.write("\n");
+            } else if (mod == 0b01000000) {
+                const rmLabel = getAddressCalc(arenaAllocator, rm) catch undefined;
+                const disp = getData(0);
+
+                if (d == 0) {
+                    _ = try aw.writer.write("[");
+                    _ = try aw.writer.write(rmLabel);
+                    _ = try aw.writer.print(" + {}", .{disp});
+                    _ = try aw.writer.write("], ");
+                    _ = try aw.writer.write(regLabel);
+                } else {
+                    _ = try aw.writer.write(regLabel);
+                    _ = try aw.writer.write(", [");
+                    _ = try aw.writer.write(rmLabel);
+                    _ = try aw.writer.print(" + {}", .{disp});
+                    _ = try aw.writer.write("]");
+                }
+                _ = try aw.writer.write("\n");
+            } else if (mod == 0b10000000) {
+                const rmLabel = getAddressCalc(arenaAllocator, rm) catch undefined;
+                const disp = getData(1);
+
+                if (d == 0) {
+                    _ = try aw.writer.write("[");
+                    _ = try aw.writer.write(rmLabel);
+                    _ = try aw.writer.print(" + {}", .{disp});
+                    _ = try aw.writer.write("], ");
+                    _ = try aw.writer.write(regLabel);
+                } else {
+                    _ = try aw.writer.write(regLabel);
+                    _ = try aw.writer.write(", [");
+                    _ = try aw.writer.write(rmLabel);
+                    _ = try aw.writer.print(" + {}", .{disp});
+                    _ = try aw.writer.write("]");
+                }
+                _ = try aw.writer.write("\n");
+            }
+        } else if ((byte1 & 0b11111100) == 0b00000000) {
+            //add imm + rm
+
+        } else if ((byte1 & 0b11111100) == 0b00000000) {
+            //add imm + accumulator
         }
-        //std.debug.print("{s}", .{buffer[0..writer.end]});
-        buffer = aw.toArrayList();
-        std.debug.print("{s}", .{buffer.items});
     }
-
-    //const value: u8 = @intCast(file[0]);
-}
-
-var instructions: []u8 = undefined;
-var instructionIndex: usize = 0;
-pub fn getNext() ?u8 {
-    if (instructionIndex >= instructions.len) {
-        return null;
-    }
-    const instruction = instructions[instructionIndex];
-    instructionIndex += 1;
-    return instruction;
-}
-
-//TODO: @finish
-const register_table = [2][8][]const u8{
-    // w = 0 (8-bit registers)
-    .{ "al", "cl", "dl", "bl", "ah", "ch", "dh", "bh" },
-    // w = 1 (16-bit registers)
-    .{ "ax", "cx", "dx", "bx", "sp", "bp", "si", "di" },
 };
-pub fn getRegister(allocator: std.mem.Allocator, reg: usize, w: u8) ![]u8 {
-    //allocation not necesery, just want to get used to arenas
-    const register = register_table[w][reg];
-    const result = try std.fmt.allocPrint(allocator, "{s}", .{register});
-    return result;
-}
-
-pub fn printArgs(args: [][:0]u8) void {
-    for (args) |arg| {
-        std.debug.print("{s}\n", .{arg});
-    }
-}
