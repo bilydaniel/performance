da06721c3f358967a2551148efc9f9c0978fe918
HP <daniel.bily@firma.seznam.cz>
2026-05-19 20:20:11 +0200

    asd

---
+++ haversine/src/generator.zig
@@ -0,0 +1,137 @@
+const std = @import("std");
+const math = std.math;
+
+const EARTH_RADIUS = 6372.8; // KM
+const Point = struct {
+    x: f64,
+    y: f64,
+};
+
+fn randomInRange(min: f64, max: f64) f64 {
+    const value = std.crypto.random.float(f64);
+    return (min + (value * (max - min)));
+}
+
+fn generateValueCenter(center: f64, radius: f64, max: f64) f64 {
+    const min = -max;
+
+    var minValue = center - radius;
+    if (minValue < min) {
+        minValue = min;
+    }
+
+    var maxValue = center + radius;
+    if (maxValue > max) {
+        maxValue = max;
+    }
+
+    const result = randomInRange(min, max);
+    return result;
+}
+
+fn haversine(x0: f64, y0: f64, x1: f64, y1: f64, radius: f64) f64 {
+    const dy = dgrToRad(y1 - y0);
+    const dx = dgrToRad(x1 - x0);
+    const y0_rad = dgrToRad(y0);
+    const y1_rad = dgrToRad(y1);
+
+    const rootTerm = (math.pow(f64, math.sin(dy / 2), 2)) + math.cos(y0_rad) * math.cos(y1_rad) * (math.pow(f64, math.sin(dx / 2), 2));
+    const result = 2 * radius * math.asin(math.sqrt(rootTerm));
+    return result;
+}
+
+fn dgrToRad(dgr: f64) f64 {
+    return (dgr * (std.math.pi / 180.0));
+}
+
+const Arguments = struct {
+    uniform: bool = false,
+    seed: u64 = 0,
+    numberOfPairs: usize = 100,
+    clusterSize: usize = 25,
+};
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
+    const args = try std.process.argsAlloc(allocator);
+    const arguments = try parseArgs(args);
+
+    var file = try std.fs.cwd().createFile("pairs.json", .{});
+    defer file.close();
+
+    var buffer: [4096]u8 = undefined;
+    var fileWriter = file.writer(&buffer);
+    var writer = &fileWriter.interface;
+    _ = try writer.write("{\n\"pairs\": [\n");
+
+    var center: Point = undefined;
+    var radiusx: f64 = undefined;
+    var radiusy: f64 = undefined;
+    if (!arguments.uniform) {
+        center.x = randomInRange(-180, 180);
+        center.y = randomInRange(-90, 90);
+        radiusx = randomInRange(0, 180);
+        radiusy = randomInRange(0, 90);
+    }
+
+    var point0: Point = undefined;
+    var point1: Point = undefined;
+    var haversine_sum: f64 = 0;
+    //TODO: @finish saving results of every pair into a binary file
+    for (0..arguments.numberOfPairs) |i| {
+        if (arguments.uniform) {
+            point0.x = randomInRange(-180, 180);
+            point0.y = randomInRange(-90, 90);
+            point1.x = randomInRange(-180, 180);
+            point1.y = randomInRange(-90, 90);
+        } else {
+            point0.x = generateValueCenter(180, center.x, radiusx);
+            point0.y = generateValueCenter(90, center.y, radiusy);
+            point1.x = generateValueCenter(180, center.x, radiusx);
+            point1.y = generateValueCenter(90, center.y, radiusy);
+        }
+
+        const haversine_value = haversine(point0.x, point0.y, point1.x, point1.y, EARTH_RADIUS);
+        haversine_sum += haversine_value;
+
+        if (i == arguments.numberOfPairs - 1) {
+            //TODO: add .16 for 16 decimal places
+            try writer.print("\t\t{{\"x0\": {d},\"y0\":{d},\"x1\":{d},\"y1\":{d} }}\n", .{ point0.x, point0.y, point1.x, point1.y });
+        } else {
+            try writer.print("\t\t{{\"x0\": {d},\"y0\":{d},\"x1\":{d},\"y1\":{d} }},\n", .{ point0.x, point0.y, point1.x, point1.y });
+        }
+
+        if (!arguments.uniform and @mod(i, arguments.clusterSize) == 0) {
+            center.x = randomInRange(-180, 180);
+            center.y = randomInRange(-90, 90);
+        }
+    }
+    _ = try writer.write("]\n}");
+    try writer.flush();
+    const haversine_avg = haversine_sum / @as(f64, @floatFromInt(arguments.numberOfPairs));
+    std.debug.print("{d}\n", .{haversine_avg});
+}
+
+fn parseArgs(args: [][:0]u8) !Arguments {
+    _ = args;
+    var arguments = Arguments{};
+    arguments.seed = @abs(std.time.microTimestamp());
+    //TODO: finish arguments
+
+    // while (args.next()) |arg| {
+    //     if (std.mem.startsWith(u8, arg, "--")) {
+    //         if (std.mem.indexOf(u8, arg, "=") != null) {
+    //             var split = std.mem.splitAny(u8, arg, "=");
+    //
+    //             const key = split.next() orelse return error.InvalidArgument;
+    //
+    //             const value = split.next() orelse return error.InvalidArgument;
+    //
+    //             try argMap.put(key[2..], value);
+    //         }
+    //     }
+    // }
+    return arguments;
+}
